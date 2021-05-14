06958bc739c5be1452f07df71a3c71df2d440cbd
Atul Anand <atulanand206@gmail.com>
2021-05-14 23:20:43 +0530

    :persevere: Authorization in allowed headers.

--- auth.go
+++ auth.go
@@ -14,6 +14,7 @@ import (
 const (
 	ContentTypeKey  = "content-type"
 	Cors            = "Access-Control-Allow-Origin"
+	CorsHeaders     = "Access-Control-Allow-Headers"
 	ApplicationJson = "application/json"
 )
 
@@ -52,6 +53,7 @@ func ApplicationJsonInterceptor() MiddlewareInterceptor {
 
 func AuthenticationInterceptor() MiddlewareInterceptor {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		w.Header().Add(CorsHeaders, "authorization")
 		secret := os.Getenv("CLIENT_SECRET")
 		err := Authenticate(r, secret)
 		if err != nil {
