8636a78fac555e37be0de48e776edf712a17ddd6
Atul Anand <atulanand206@gmail.com>
2021-05-15 13:35:51 +0530

    :satisfied: Refactor and Extraction.

--- auth.go
+++ auth.go
@@ -12,10 +12,18 @@ import (
 )
 
 const (
-	ContentTypeKey  = "content-type"
-	Cors            = "Access-Control-Allow-Origin"
-	CorsHeaders     = "Access-Control-Allow-Headers"
+	Accept         = "Accept"
+	ContentTypeKey = "Content-Type"
+	CorsOrigin     = "Access-Control-Allow-Origin"
+	CorsHeaders    = "Access-Control-Allow-Headers"
+	CorsMethods    = "Access-Control-Allow-Methods"
+
 	ApplicationJson = "application/json"
+	Authorization   = "Authorization"
+
+	ClientSecret       = "CLIENT_SECRET"
+	TokenExpireMinutes = "TOKEN_EXPIRE_MINUTES"
+	ValueCorsOrigin    = "CORS_ORIGIN"
 )
 
 type MiddlewareInterceptor func(http.ResponseWriter, *http.Request, http.HandlerFunc)
@@ -37,9 +45,16 @@ func (chain MiddlewareChain) Handler(handler http.HandlerFunc) http.HandlerFunc 
 	return http.HandlerFunc(curr)
 }
 
+func CorsMethodInterceptor(methods string) MiddlewareInterceptor {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		w.Header().Set(CorsMethods, methods)
+		next(w, r)
+	}
+}
+
 func CorsAllInterceptor() MiddlewareInterceptor {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		w.Header().Set(Cors, "*")
+		w.Header().Set(CorsOrigin, os.Getenv(ValueCorsOrigin))
 		next(w, r)
 	}
 }
@@ -53,8 +68,8 @@ func ApplicationJsonInterceptor() MiddlewareInterceptor {
 
 func AuthenticationInterceptor() MiddlewareInterceptor {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		w.Header().Add(CorsHeaders, "authorization")
-		secret := os.Getenv("CLIENT_SECRET")
+		w.Header().Add(CorsHeaders, Authorization)
+		secret := os.Getenv(ClientSecret)
 		err := Authenticate(r, secret)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusForbidden)
@@ -65,14 +80,14 @@ func AuthenticationInterceptor() MiddlewareInterceptor {
 }
 
 func CreateToken(username string) (string, error) {
-	secret := os.Getenv("CLIENT_SECRET")
-	expiry, err := strconv.Atoi(os.Getenv("TOKEN_EXPIRE_MINUTES"))
+	secret := os.Getenv(ClientSecret)
+	expiry, err := strconv.Atoi(os.Getenv(TokenExpireMinutes))
 	if err != nil {
 		return "", err
 	}
 	expireTime := time.Minute * time.Duration(expiry)
 	claims := jwt.MapClaims{}
-	claims["userName"] = username
+	claims["username"] = username
 	claims["expires"] = time.Now().Add(expireTime).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	token, err := at.SignedString([]byte(secret))
@@ -83,7 +98,7 @@ func CreateToken(username string) (string, error) {
 }
 
 func Authenticate(r *http.Request, secret string) error {
-	bearerToken := r.Header.Get("Authorization")
+	bearerToken := r.Header.Get(Authorization)
 	split := strings.Split(bearerToken, " ")
 	if len(split) == 2 {
 		tokenString := split[1]
