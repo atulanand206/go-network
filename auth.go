daa664ec36e27f5232171449947a179e0249a185
Atul Anand <atulanand206@gmail.com>
2021-06-05 14:21:57 +0530

    :confused: Post method allow header.

--- auth.go
+++ auth.go
@@ -70,7 +70,7 @@ func CorsInterceptor(methods string) MiddlewareInterceptor {
 		w.Header().Set(CorsOrigin, os.Getenv(ValueCorsOrigin))
 		w.Header().Set(CorsMethods, methods)
 		w.Header().Add(CorsHeaders, Authorization)
-		if r.Method == http.MethodPost {
+		if methods == http.MethodPost {
 			w.Header().Add(CorsHeaders, ContentTypeKey)
 		}
 		if r.Method != http.MethodOptions {
